5b9777057c75c730e6908e7b58a3fa4724bf4999
gloireisesa <isesagloire0@gmail.com>
2024-07-29 01:56:33 +0200

    modification de fonction

--- .vscode/creer_table.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "table.h"
-
-void creer_table(char *name, int columns, char *column_defs[]) {
-    // ... (votre code pour créer une table)
-}
--- .vscode/inserer_dans_table.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include "table.h"
-
-void inserer_dans_table(char *name, char *values[]) {
-    // ... (votre code pour insérer des données)
-}
