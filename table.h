c27609dedc890b13dc66dd1198f4a66e75d26f51
Patricilunga <patricilunga99@gmail.com>
2024-08-03 15:05:37 +0200

    Implementation de la fonction pour sélectionner

--- table.h
+++ table.h
@@ -21,6 +21,8 @@ typedef struct {
     char *data[MAX_ROWS][MAX_COLUMNS];
     int primary_key_index;
 } Table;
+Table tables[MAX_TABLES];
+int table_count = 0;
 
 // Déclarations des fonctions
 void creer_table(char *name, int columns, char *column_defs[]);
